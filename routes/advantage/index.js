d3c4adf88e26425a0faabb254a0131a3242b7ffa
Caicke <caicke@gmail.com>
2022-07-19 23:44:59 -0300

    Criando arquivo com funções compartilhadas

--- routes/advantage/index.js
+++ routes/advantage/index.js
@@ -13,11 +13,51 @@ router.route("/:name").get(async (req, res) => {
     try {
       const pokemonName = req.params.name;
       
-      
+      await axios.get(url)
+      .then(r => {
+        if (r.status == 200) {
+            const types = getPokemonTypes(r.data.pokemon, pokemonName);
+
+            const dominatedPokemons = getPokemonWeaknessesByName(r.data.pokemon, types);
+
+            if (dominatedPokemons) {
+                res.status(200).send(dominatedPokemons);
+            }
+            else {
+                res.status(404).send("Pokemon not found.");
+            }
+        }
+        else {
+            res.status(400).send(r);
+        }
+      });
+
     } catch (err) {
       res.status(400).json({ error: err.message });
     }
 });
 
+const getPokemonWeaknessesByName = (pokemonList, typeList) => {
+    const pokemons = []
+
+    pokemonList.map(ele => {
+        if (typeList.some(r => ele.weaknesses.indexOf(r) >= 0)) {
+            pokemons.push(ele);
+        }
+    });
+
+    return pokemons;
+}
+
+const getPokemonTypes = (pokemonList, name) => {
+    var types = [];
+    const pokemon = pokemonList.find(element => element.name.toUpperCase() === name.toUpperCase());
+    
+    if (pokemon) {
+        types = pokemon.type;
+    }
+
+    return types;
+}
 
 module.exports = router;
--- routes/weakness/index.js
+++ routes/weakness/index.js
@@ -11,14 +11,56 @@ router.route("/").get( async (req, res) => {
 
 router.route("/:name").get(async (req, res) => {
     try {
-      const pokemonName = req.params.name;
-      
-      
-    } catch (err) {
-      res.status(400).json({ error: err.message });
-    }
+        const pokemonName = req.params.name;
+        
+        await axios.get(url)
+        .then(r => {
+          if (r.status == 200) {
+              const pokemon = getPokemonByName(r.data.pokemon, pokemonName);
+              
+              if (!pokemon) {
+                res.status(404).send("Pokémon not found.");
+              }
+              
+              const types = getPokemonTypes(pokemon);
+  
+              const dominatingPokemons = getPokemonAdvantagesByType(r.data.pokemon, types);
+  
+              if (dominatingPokemons) {
+                  res.status(200).send(dominatingPokemons);
+              }
+              else {
+                  res.status(404).send("Pokémon not found.");
+              }
+          }
+          else {
+              res.status(400).send(r);
+          }
+        });
+  
+      } catch (err) {
+        res.status(400).json({ error: err.message });
+      }
 });
 
+const getPokemonByName = (pokemonList, name) => {
+    return pokemonList.find(element => element.name.toUpperCase() === name.toUpperCase())
+}
 
+const getPokemonAdvantagesByType = (pokemonList, typeList) => {
+    const pokemons = []
+
+    pokemonList.map(ele => {
+        if (!typeList.some(r => ele.weaknesses.indexOf(r) >= 0)) {
+            pokemons.push(ele);
+        }
+    });
+
+    return pokemons;
+}
+
+const getPokemonTypes = (pokemon) => {
+    return pokemon.type;
+}
 
 module.exports = router;
